67068c89da4472b5e07ab1527275f1a36e38a5d0
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-12-01 12:01:24 -0600

    fixed submit event

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 //variables 
 const ratingsContainer = document.getElementById('ratings-container');
 const thankYouContainer = document.getElementById("thank-you-container");
+const ul = document.querySelector('.ul');
+const rating = document.getElementById('rating');
+const submitButton = document.querySelector('.btn-submit');
+
 
 //choosing the rating
-let rating = document.getElementById('rating')
-const ul = document.querySelector('.ul');
 
-ul.addEventListener('click' , function(e){
+ul.addEventListener('click' , (e) => {
     rating.innerText = e.target.innerText;
-    for(let i = 0; i < ul.children.length; i++) {
-        if(ul.children[i].id != rating.innerText) {
-            ul.children.style.backgroundColor = "hsl(0, 0%, 100%)";
-            ul.children.style.backgroundColor = "hsl(25, 97%, 53%)";
-            e.target.style.backgroundColor = "hsl(25, 97%, 53%)";
-            e.target.style.color = "hsl(0, 0%, 100%)";
-        }
-    }
+    console.log(rating.innerText)
 })
+
+const toggleDisplay = () => {
+    if(rating.innerText){
+        ratingsContainer.style.display = "none"
+    }
+}
 
 
 
